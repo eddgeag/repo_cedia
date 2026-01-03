273fd6e32b428423c3a28c51bb897bdee58449bc
edmond geraud <eddgeag@gmail.com>
2026-01-03 16:14:03 -0500

    funcionar

--- pipeline_lab.R
+++ pipeline_lab.R
@@ -991,39 +991,44 @@ variantFiltration <- function(folder_fasta, output_dir, fastq_dir) {
   ## =========================
   
   if (!file.exists(snps_filt_vcf)) {
+    
     args_snps <- c(
       "-R", fasta_file,
       "-V", snps_vcf,
       "-O", snps_filt_vcf,
       
       "--filter-name", "QD2",
-      "--filter-expression", "\"QD < 2.0\"",
+      "--filter-expression", "QD < 2.0",
       
       "--filter-name", "FS60",
-      "--filter-expression", "\"FS > 60.0\"",
+      "--filter-expression", "FS > 60.0",
       
       "--filter-name", "MQ40",
-      "--filter-expression", "\"MQ < 40.0\"",
+      "--filter-expression", "MQ < 40.0",
       
       "--filter-name", "MQRS12",
-      "--filter-expression", "\"MQRankSum < -12.5\"",
+      "--filter-expression", "MQRankSum < -12.5",
       
       "--filter-name", "RPRS8",
-      "--filter-expression", "\"ReadPosRankSum < -8.0\"",
+      "--filter-expression", "ReadPosRankSum < -8.0",
       
       "--filter-name", "SOR3",
-      "--filter-expression", "\"SOR > 3.0\""
+      "--filter-expression", "SOR > 3.0"
     )
-    
     
     argfile <- tempfile(fileext = ".args")
     writeLines(args_snps, argfile)
+    
+    cat("\n### GATK VariantFiltration (SNPs) ###\n")
+    cat("Arguments file:", argfile, "\n")
+    cat("---- contents ----\n")
+    cat(readLines(argfile), sep = "\n")
+    cat("\n------------------\n")
     
     system2(
       gatk_bin,
       args = c("VariantFiltration", "--arguments_file", argfile)
     )
-
   }
   
   
