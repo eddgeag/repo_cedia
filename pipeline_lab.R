13976825ede25715c02fcb7346cac87870c34c91
edmond geraud <eddgeag@gmail.com>
2026-01-03 16:11:53 -0500

    funcionar

--- pipeline_lab.R
+++ pipeline_lab.R
@@ -997,23 +997,24 @@ variantFiltration <- function(folder_fasta, output_dir, fastq_dir) {
       "-O", snps_filt_vcf,
       
       "--filter-name", "QD2",
-      "--filter-expression", "QD < 2.0",
+      "--filter-expression", "\"QD < 2.0\"",
       
       "--filter-name", "FS60",
-      "--filter-expression", "FS > 60.0",
+      "--filter-expression", "\"FS > 60.0\"",
       
       "--filter-name", "MQ40",
-      "--filter-expression", "MQ < 40.0",
+      "--filter-expression", "\"MQ < 40.0\"",
       
       "--filter-name", "MQRS12",
-      "--filter-expression", "MQRankSum < -12.5",
+      "--filter-expression", "\"MQRankSum < -12.5\"",
       
       "--filter-name", "RPRS8",
-      "--filter-expression", "ReadPosRankSum < -8.0",
+      "--filter-expression", "\"ReadPosRankSum < -8.0\"",
       
       "--filter-name", "SOR3",
-      "--filter-expression", "SOR > 3.0"
+      "--filter-expression", "\"SOR > 3.0\""
     )
+    
     
     argfile <- tempfile(fileext = ".args")
     writeLines(args_snps, argfile)
@@ -1022,7 +1023,6 @@ variantFiltration <- function(folder_fasta, output_dir, fastq_dir) {
       gatk_bin,
       args = c("VariantFiltration", "--arguments_file", argfile)
     )
-    
 
   }
   
